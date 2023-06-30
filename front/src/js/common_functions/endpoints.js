3bd2c2365d2d4b0bd1095283ee6dc1adda9d8c51
pawelprzegon <p.przegon@gmail.com>
2023-07-01 00:21:05 +0200

    changed url with api endpoint

--- front/src/js/common_functions/endpoints.js
+++ front/src/js/common_functions/endpoints.js
@@ -1,31 +1,31 @@
-let url = "http://localhost:8001/";
+let url = 'https://dur-logs-back.onrender.com/';
 
 export async function callApiGet(path) {
   try {
     let resp = await fetch(url + path, {
-      method: "GET",
-      credentials: "include",
+      method: 'GET',
+      credentials: 'include',
     });
     return [resp.status, await resp.json()];
   } catch (error) {
-    console.log("error: " + error);
-    return ["error", error];
+    console.log('error: ' + error);
+    return ['error', error];
   }
 }
 
 export async function callApiPut(path) {
   const myHeaders = new Headers({
-    accept: "application/json",
+    accept: 'application/json',
   });
   try {
     let resp = await fetch(url + path, {
-      method: "PUT",
-      credentials: "include",
+      method: 'PUT',
+      credentials: 'include',
       headers: myHeaders,
     });
     return [await resp.json(), resp.status];
   } catch (error) {
-    console.log("error: " + error);
-    return ["error", error];
+    console.log('error: ' + error);
+    return ['error', error];
   }
 }
